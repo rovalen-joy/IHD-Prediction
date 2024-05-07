0a117a713052882c30242915bcec756f97adfdf0
David Bryan Barcelona <davidbryan.barcelona@wvsu.edu.ph>
2024-05-07 15:49:37 +0800

    Created the Default page

---
+++ ihd-prediction-system/src/pages/Prediction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
